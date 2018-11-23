344146a272b8df0c8041dc83a39f35c3b5b3105a
kevaday <kevaday123@gmail.com>
2018-11-23 18:51:56 -0500

    bug fixes and fixed compatibility with model.py

--- sample.py
+++ sample.py
@@ -3,12 +3,15 @@
 
 import argparse
 
-from model import CharRNN, sample
+from model import load_model, sample
+import torch
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('model', type=str, default=None, help='checkpointed model to sample from')
     parser.add_argument('--cuda', action='store_true', default=False, help='run the network on the GPU with CUDA')
+    parser.add_argument('--fastest', action='store_true', default=False, help='set CuDNN to fastest mode')
+    parser.add_argument('--benchmark', action='store_true', default=False, help='use CuDNN benchmark mode to find optimal algorithms')
     parser.add_argument('--length', type=int, default=256, help='number of characters to sample from the model')
     parser.add_argument('--prime', type=str, default='The ', help='start sampling from network with these starting characters')
     parser.add_argument('--top_k', type=int, default=10, help='find most probable next char from the output softmax with top_k')
@@ -17,7 +20,16 @@ def main():
 
     args = parser.parse_args()
 
-    model = CharRNN.load(args.model, args.cuda)
+    model = load_model(args.model)
+    if args.cuda:
+        model.cuda()
+    else:
+        model.cpu()
+        
+    if args.fastest:
+        torch.backends.cudnn.fastest = True
+    if args.benchmark:
+        torch.backends.cudnn.benchmark = True
 
     if not args.savefile:
         print(sample(model, length=args.length, prime=args.prime, cuda=args.cuda, top_k=args.top_k))
